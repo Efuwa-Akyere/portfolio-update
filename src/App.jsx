026f8bd3ea86b6d2c1c54071351eb1f92aa3befd
Efuwa Akyere <efuwayorke13@gmail.com>
2024-10-18 13:12:08 +0000

    corrected error in component name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import About from './components/About'
-import Contact from './components/contact'
+import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
